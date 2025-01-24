d9b4dcd41025cd265526318435b6808005480259
Karam Sawalha <89216965+ksawalha@users.noreply.github.com>
2025-01-24 23:30:43 +0300

    Update CallNoSleep.js

--- www/CallNoSleep.js
+++ www/CallNoSleep.js
@@ -1,17 +1,23 @@
 var exec = require('cordova/exec');
 
 var CallNoSleep = {
-    startCall: function(phoneNumber, successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'CallNoSleep', 'startCall', [phoneNumber]);
+    callNumber: function(successCallback, errorCallback, number, bypassAppChooser) {
+        exec(successCallback, errorCallback, "CallNoSleep", "callNumber", [number, bypassAppChooser]);
+    },
+    isCallSupported: function(successCallback) {
+        exec(successCallback, null, "CallNoSleep", "isCallSupported", []);
+    },
+    startCall: function(successCallback, errorCallback, number) {
+        exec(successCallback, errorCallback, "CallNoSleep", "startCall", [number]);
     },
     preventSleep: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'CallNoSleep', 'preventSleep', []);
+        exec(successCallback, errorCallback, "CallNoSleep", "preventSleep", []);
     },
     allowSleep: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'CallNoSleep', 'allowSleep', []);
+        exec(successCallback, errorCallback, "CallNoSleep", "allowSleep", []);
     },
     getAppInfo: function(successCallback, errorCallback) {
-        exec(successCallback, errorCallback, 'CallNoSleep', 'getAppInfo', []);
+        exec(successCallback, errorCallback, "CallNoSleep", "getAppInfo", []);
     }
 };
 
